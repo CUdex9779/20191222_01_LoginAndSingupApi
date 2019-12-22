23df8d0bc06c7b0f08cc84040fe443dc21972a3f
CUdex9779 <cuyu9779@gmail.com>
2019-12-22 17:03:01 +0900

    API호출 후 로그만 확인

--- app/src/main/java/com/example/a20191222_01_loginandsingupapi/EditBlackListActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsingupapi/EditBlackListActivity.kt
@@ -2,7 +2,10 @@ package com.example.a20191222_01_loginandsingupapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.a20191222_01_loginandsingupapi.Utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_edit_black_list.*
+import org.json.JSONObject
 
 class EditBlackListActivity : BaseActivity() {
 
@@ -24,6 +27,16 @@ class EditBlackListActivity : BaseActivity() {
             val content = contentEdt.text.toString()
 
 //            서버에 게시글 등록을 요청하는 기능을 추가해야함
+
+//            서버에 등록 API를 호출하는 코드
+            ConnectServer.postRequestBlackList(mContext,title,phone,content,object : ConnectServer.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+
+                    Log.d("게시글등록응답",json.toString())
+
+                }
+
+            })
 
         }
 
