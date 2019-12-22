12ab369616b21af17403eb4e3c125485d136137b
CUdex9779 <cuyu9779@gmail.com>
2019-12-22 13:54:45 +0900

    로그인 기능 추가/실패시 왜 실패인지 알려주는 기능 추가

--- app/src/main/java/com/example/a20191222_01_loginandsingupapi/LoginActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsingupapi/LoginActivity.kt
@@ -2,7 +2,11 @@ package com.example.a20191222_01_loginandsingupapi
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.a20191222_01_loginandsingupapi.Utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -16,6 +20,32 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setUpEvents() {
+
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            ConnectServer.postRequestLogin(mContext,inputId,inputPw,object : ConnectServer.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    Log.d("로그인 응답",json.toString())
+
+                    val code = json.getInt("code")
+
+                    if (code == 200){
+
+                    }
+                    else{
+                        val message = json.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                }
+
+            })
+        }
 
 
         signUpBtn.setOnClickListener {
